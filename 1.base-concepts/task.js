07bb92cb6aab5133970b2c6a97f9e1f1d2694772
dmiweb <dimbons@mail.ru>
2023-10-21 18:35:52 +0300

    Реализовано первое задание base-concepts

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,25 @@
 "use strict"
 function solveEquation(a, b, c) {
+  let d = b ** 2 - 4 * a * c;
   let arr = [];
-  
+  if (d < 0) {
+    arr = [];
+  }
+  if (d === 0) {
+    arr = [-b / (2 * a)];
+  }
+  if (d > 0) {
+    arr = [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)];
+  }
+
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  percent = percent / 100 / 12;
+  let loanBody = Math.abs(amount - contribution);
+  let monthlyPayment = loanBody * (percent + (percent / (((1 + percent) ** countMonths) - 1)));
+  let totalAmount = Number((monthlyPayment * countMonths).toFixed(2));
+
+  return totalAmount;
 }
