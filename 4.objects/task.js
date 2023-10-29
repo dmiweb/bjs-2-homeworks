1f46cb1041445751515ca71790199688bc6c253f
dmiweb <dimbons@mail.ru>
2023-10-29 23:56:44 +0300

    Четвертое задание готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,33 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
+
+let student1 = new Student("Василиса", "женский", 19);
+let student2 = new Student("Артём", "мужской", 25);
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  return this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function (...marksToAdd) {
+  if (this.marks !== undefined) {
+    this.marks.push(...marksToAdd);
+  }
 }
 
 Student.prototype.getAverage = function () {
-  
+  if (this.marks === undefined || this.marks == []) {
+    return 0;
+  } else {
+    return this.marks.reduce((acc, item) => acc + item / this.marks.length, 0);
+  }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject;
+  delete this.marks;
+  return this.excluded = reason;
 }
