217d6439519c747291612a1fa85b2ec9aedd8772
dmiweb <dimbons@mail.ru>
2023-11-09 17:18:42 +0300

    Выполнено шестое задание

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,55 @@
-﻿
+﻿function parseCount(value) {
+  const parseResult = Number.parseFloat(value);
+  if (isNaN(parseResult)) {
+    throw new Error("Не валидное значение");
+  }
+
+  return parseResult;
+}
+
+function validateCount(value) {
+  try {
+    return parseCount(value);
+  } catch (error) {
+
+    return error;
+  }
+}
+
+class Triangle {
+  constructor(a, b, c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+
+    if (this.a + this.b < this.c ||
+      this.a + this.c < this.b ||
+      this.b + this.c < this.a) {
+      throw new Error("Треугольник с такими сторонами не существует");
+    }
+  }
+
+  get perimeter() {
+    return this.a + this.b + this.c;
+  }
+
+  get area() {
+    let semiP = this.perimeter / 2;
+    return Math.sqrt(semiP * (semiP - this.a) * (semiP - this.b) * (semiP - this.c)).toFixed(3);
+  }
+}
+
+function getTriangle(a, b, c) {
+  try {
+    return new Triangle(a, b, c);
+  } catch (error) {
+    return {
+      get perimeter() {
+        return "Ошибка! Треугольник не существует";
+      },
+      get area() {
+        return "Ошибка! Треугольник не существует";
+      }
+    }
+  }
+}
